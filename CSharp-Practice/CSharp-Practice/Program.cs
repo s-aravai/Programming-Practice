452d224bfeae01c4157f357206c84ac72dd1e01e
dondonherrera <dondon.herrera@kobkiat-it.com>
2025-01-15 15:18:20 -0700

    Introduction

--- CSharp-Practice/CSharp-Practice/Program.cs
+++ CSharp-Practice/CSharp-Practice/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp_Practice
 {
@@ -10,8 +6,53 @@ namespace CSharp_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello Salman and Victor!");
+            int x = 5;
+            int y = 5;
+            Console.WriteLine("Display terms: ");
+            Console.WriteLine($"${x}+{y}={x + y}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter two numbers: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"${a}+{b}={a + b}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter two numbers in OOP");
+            Addition addObject = new Addition();
+            addObject.SetX(Console.ReadLine());
+            addObject.SetY(Console.ReadLine());
+            addObject.Display();
         }
     }
+
+    internal class Addition
+    {
+        int x;
+        int y;
+
+        public void SetX(string val)
+        {
+            x = int.Parse(val);
+        }
+
+        public void SetY(string val)
+        {
+            y = int.Parse(val);
+        }
+
+        private int GetSummation()
+        {
+            return x + y;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{x}+{y}={GetSummation()}");
+        }
+    }
+
+
 }
